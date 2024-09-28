5989f1a98d63d24d6de17eba0227107c65abdaf6
Exe <exegozategui@gmail.com>
2024-09-27 22:04:22 -0300

    first commit

---
+++ src/data/experience.ts
@@ -0,0 +1,60 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "Septiembre 2024 — Presente",
+    title: "Software Developer",
+    company: "Asterbizz",
+    companyUrl: "https://www.asterbizz.com",
+    description:
+      "Soy cofundador de Asterbizz, una empresa que fundé junto a dos colegas, donde nos especializamos en el desarrollo de páginas web y software empresarial. Nuestro enfoque está en ofrecer soluciones tecnológicas personalizadas que ayuden a nuestros clientes a optimizar sus procesos y alcanzar sus objetivos.",
+    technologies: [
+      "Next.js",
+      "Tailwind CSS",
+      "React",
+      "Node.JS",
+      "SQL",
+      "API",
+
+
+
+    ],
+    logo: "/logos/aster.jpeg",
+  },
+  {
+    dates: "Agosto 2023 — Presente",
+    title: "Front End Developer",
+    company: "Somos Firca",
+    companyUrl: "https://somosfirca.org/",
+    description:
+      "Actualmente formo parte de la Fundación Somos Firca, una organización dedicada a la reforestación. Colaboro en el diseño y desarrollo de su página web, aplicando metodologías ágiles para garantizar que la plataforma refleje adecuadamente la misión y valores de la fundación, mientras apoyo en la mejora continua del sitio.",
+    technologies: [
+      "HTML",
+      "Javascritp",
+      "CSS",
+      "Jquery",
+      "Firebase",
+      "Scrum",
+      
+    ],
+    logo: "/logos/logo verde.png",
+  },
+  {
+    dates: "Marzo 2021 — Presente",
+    title: "Tecnicatura en Desarrollo de Software",
+    company: "IES 9-023",
+    companyUrl: "https://ies9023-infd.mendoza.edu.ar/sitio/",
+    description:
+      "Soy estudiante avanzado en la carrera de Técnico en Desarrollo de Software, donde he adquirido una sólida base en programación, desarrollo web y bases de datos. Mi formación incluye el uso de metodologías ágiles y un enfoque práctico en la resolución de problemas.",
+    technologies: ["Fundamentos de Programación",
+      "Lógica Computacional",
+      "Arquitectura de Computadoras",
+      "Modelado de Software",
+      "Bases de Datos",
+      "Seguridad Informática",
+      "Auditoría y Calidad de Software"
+    ],
+    logo: "/logos/ies.png",
+  },
+
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,23 @@
+
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconTwitter from "../components/icons/twitter.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/Exegozategui",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/exegozategui/",
+    icon: IconLinkedin,
+  },
+  {
+    name: "Twitter",
+    url: "",
+    icon: IconTwitter,
+  },
+
+];
